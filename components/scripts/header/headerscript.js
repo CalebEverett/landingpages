24472dbbf609588a90115de0ab070decfea5dd63
Caleb Everett <caleb@careercalifornia.edu>
2015-02-10 01:02:04 -0800

    minor tweaks

--- components/scripts/header/headerscript.js
+++ components/scripts/header/headerscript.js
@@ -1,10 +1,5 @@
   WebFont.load({
   	google: {
-  		families: ['Slabo+27px','Roboto:600,400,300']
+  		families: ['Slabo+27px','Roboto:600,300','Permanent+Marker']
   	}
   });
-
-(function(a,e,c,f,g,b,d){var h={ak:"1021182766",cl:"HynjCNC_uFkQrob45gM"};a[c]=a[c]||function(){(a[c].q=a[c].q||[]).push(arguments)};a[f]||(a[f]=h.ak);b=e.createElement(g);b.async=1;b.src="//www.gstatic.com/wcm/loader.js";d=e.getElementsByTagName(g)[0];d.parentNode.insertBefore(b,d);a._googWcmGet=function(b,d,e){a[c](2,b,h,d,null,new Date,e)}})(window,document,"_googWcmImpl","_googWcmAk","script");
-
-
-
