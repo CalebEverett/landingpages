6b4b831aab9e691dd1b2985f6335ba2301c66d4a
Caleb Everett <caleb@careercalifornia.edu>
2015-02-15 20:42:07 -0800

    added intro animations

--- components/scripts/footer/footerscript.js
+++ components/scripts/footer/footerscript.js
@@ -1,7 +1,18 @@
-//comment
-$(function() {
+//show page loader while page loads
+$(window).load(function() {
+    // Animate loader off screen
+    $(".loader").fadeOut("slow");
+  });
+
 
 $(function() {
+
+
+//screensize breakpoints
+var narrow = 400,
+    small = 600,
+    medium = 960,
+    wide = 1200;
 
 //window height
   var wheight = $(window).height()-$('#nav').height(); //get height of the window
@@ -9,12 +20,16 @@ $(function() {
   $('.fullheight').css('height', wheight);
 
   $(window).resize(function() {
-    var wheight = $(window).height()-$('#nav').height();; //get height of the window
+    var wheight = $(window).height()-$('#nav').height(); //get height of the window
     $('.fullheight').css('height', wheight);
   }) //on resize
 
-});
+  //window width
+  var wwidth = $(window).width(); //get width of the window
 
+  $(window).resize(function() {
+    var wwidth = $(window).width() //get width of the window
+  }) //on resize
 
    //set up ScrollMagic
   var controller = new ScrollMagic({
@@ -28,6 +43,39 @@ $(function() {
     triggerElement: '#nav',
   }).setPin('#nav').addTo(controller);
 
+//animate intro call to action
+var t1 = new TimelineMax();
+
+if(wwidth <= small) {
+
+  t1.from(".introcta", 0.25, {opacity:0,delay:0.5})
+  .from(".introcta", 1.5, {marginBottom: wheight-$('.titletext').height()-$('.introcta').height(),ease: Bounce.easeOut});
+}
+
+else if (wwidth > small) {
+  t1.from(".introcta", 0.5, {opacity:0,delay:0.5})
+  .from(".introtext", 0.5, {opacity:0,delay:-.25})
+  .from("#handarrow", 0.5, {opacity:0,delay:-.25})
+  .from(".introtext", 1.5, {marginRight: wwidth/2, ease:Bounce.easeOut});
+
+}
+
+//smooth scrolling
+$(function() {
+  $('a[href*=#]:not([href*=#modal])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top-$('#nav').height()
+        }, 800);
+        return false;
+      }
+    }
+  });
+});
+
 //not a touch device
   var isTouch = 'ontouchstart' in document.documentElement;
   if(!isTouch){
@@ -37,7 +85,8 @@ $('strong').each(function(){
   var currentStrong = $(this);
 
   var tweenStrong = new TimelineMax()
-  .from(currentStrong, 0.25, {css: {fontWeight: 300}});
+  .from(currentStrong, 0.25, {fontWeight: 300})
+  .to(currentStrong, 0.25, {textShadow:"5px 0px 10px yellow, 0px 5px 10px yellow, -5px 0px 10px yellow, 0px -5px 10px yellow"});
 
   var scene = new ScrollScene({triggerElement: currentStrong, offset: -$(window).height()*0.75})
   .setTween(tweenStrong)
@@ -66,6 +115,9 @@ $('.testimonial').each(function(){
   .addTo(controller);
 
 });//animate testimonials
+
+
+
 
   }//not a touch device
 
